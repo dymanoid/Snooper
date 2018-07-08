e0e1f6f5f5ab4a3833d1513c4b9e4501823ea896
dymanoid <9433345+dymanoid@users.noreply.github.com>
2018-07-08 19:56:25 +0200

    Create the project

---
+++ src/Snooper/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Snooper")]
+[assembly: AssemblyDescription("A mod for the Cities:Skylines game")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("dymanoid")]
+[assembly: AssemblyProduct("Snooper")]
+[assembly: AssemblyCopyright("Copyright © 2018, dymanoid")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
